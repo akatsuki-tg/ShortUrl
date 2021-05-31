a1d6a2afe97ae201ace6ec9b555336fdb35a3acf
akatsuki-tg <85111768+akatsuki-tg@users.noreply.github.com>
2021-05-31 15:07:59 +0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+banner() {
+clear
+printf "\e[0m\n"
+printf "\e[0m\e[1;91m    _____ _                _     _    _ _____  _      \e[0m\n"
+printf "\e[0m\e[1;91m   / ____| |              | |   | |  | |  __ \| |     \e[0m\n"
+printf "\e[0m\e[1;91m  | (___ | |__   ___  _ __| |_  | |  | | |__) | |     \e[0m\n"
+printf "\e[0m\e[1;91m   \___ \|  _ \ / _ \|  __| __| | |  | |  _  /| |     \e[0m\n"
+printf "\e[0m\e[1;91m   ____) | | | | (_) | |  | |_  | |__| | | \ \| |____ \e[0m\n"
+printf "\e[0m\e[1;91m  |_____/|_| |_|\___/|_|  \___|  \____/|_|  \_\______|\e[0m\n"
+printf "\e[0m\n"
+printf "\e[0m\e[1;92m        Created By qwadoh \e[0m\e[1;91m( \e[0m\e[1;92mqwadoh \e[0m\e[1;91m)\e[0m\n"
+printf "\e[0m\n"
+printf "\e[0m\e[1;93m    Contact :\e[0m\e[1;96m qwadohzero \e[0m\e[1;97m| \e[0m\e[1;93mInstagram :\e[0m\e[1;96m qwadoh\e[0m\n"
+}
+banner
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Installing Packages..\e[0m\n"
+printf "\e[0m\n"
+apt update
+apt install curl -y
+banner
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Creating Environment...\e[0m\n"
+printf "\e[0m\n"
+cp -f url /data/data/com.termux/files/usr/bin/shorturl
+cp -f url /data/data/com.termux/files/usr/bin/ShortUrl
+echo 'termux-open-url https://github.com/akatsuki-tg/' > /data/data/com.termux/files/usr/bin/akatsuki-tg
+echo 'termux-open-url https://github.com/akatsuki-tg/' > /data/data/com.termux/files/usr/bin/akatsuki-tg
+echo 'termux-open-url https://www.instagram.com/qwadoh/' > /data/data/com.termux/files/usr/bin/instagram
+echo 'termux-open-url https://www.instagram.com/qwadoh/' > /data/data/com.termux/files/usr/bin/Instagram
+chmod +x /data/data/com.termux/files/usr/bin/qwadoh
+chmod +x /data/data/com.termux/files/usr/bin/qwadoh
+chmod +x /data/data/com.termux/files/usr/bin/instagram
+chmod +x /data/data/com.termux/files/usr/bin/Instagram
+chmod +x /data/data/com.termux/files/usr/bin/shorturl
+chmod +x /data/data/com.termux/files/usr/bin/ShortUrl
+sleep 2
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Installation Succeed !!\e[0m\n"
+sleep 2
+printf "\e[0m\n"
+printf "\e[0m\n"
+printf " \e[0m\e[1;91m[\e[0m\e[1;97m~\e[0m\e[1;91m]\e[0m\e[1;93m Type \e[0m\e[1;92mShortUrl \e[0m\e[1;93mto Run this Tool \e[0m\n"
+printf "\e[0m\n"
+printf "\e[0m\n"
